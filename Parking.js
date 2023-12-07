9f4ec4fb1ed076dd6f88cdca41a7a5ee38c6d899
Ragnarlothbrokraj <152988785+Ragnarlothbrokraj@users.noreply.github.com>
2023-12-07 16:18:26 +0530

    Create Parking.js

---
+++ Parking.js
@@ -0,0 +1,53 @@
+async function postData(url = '', auth, data = {}) {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Authorization': auth,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  };
+  
+  // method to sha256 encode string
+  async function sha256(message) {
+    const msgBuffer = new TextEncoder('utf-8').encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+  }
+async function selectedSeat(cubicleID) {
+      alert("Seat " + cubicleID +" already selected please choose another seat:");
+    }
+selectSeat = async function(cubicleID) {
+    const seatId = cubicleID;
+  console.log(seatId);
+    
+   const queryParams = new URLSearchParams(document.location.search);
+    const userId = queryParams.get('userId');
+    const conversationId = queryParams.get('convId');
+    const botId = queryParams.get('botId');
+  
+    // use correct domain for your region
+    const domain = 'https://va.bc-intg.liveperson.net/thirdparty-services-0.1/webview';
+    
+    // encode auth string
+    const authString = `${conversationId} || ${botId}`;
+    const auth = await sha256(authString);
+  
+    const res = await postData(domain, auth, {
+      botId,
+      conversationId,
+      userId,
+      message: "OverDueConv", // optional
+      contextVariables: [
+         
+        {"name": "seatId", "value": seatId}
+       
+         
+      ],
+    });
+       window.close();     
+ }
